cfec3013ed3167c58ea8b7b313815dc9ce7a8885
tyler <trr4652@rit.edu>
2017-01-10 17:37:17 -0500

    Finished implementation, removed print statements, removed

--- stooge/src/main.rs
+++ stooge/src/main.rs
@@ -1,10 +1,17 @@
 use std::fmt::Debug;
-pub fn sort<T: PartialOrd >(data: &mut [T]) where T: Debug{
-	print_vec(data);
+pub fn sort<T: PartialOrd>(data: &mut [T]){
 	let n = data.len();
-	if data[0] < data[n - 1] {
+	if data[0] > data[n - 1] {
 		data.swap(0, n - 1);
 	}
+	if n > 2 {
+		let m = ((n) as f32/3.0).ceil();
+		let m2 = (n as f32 - m).ceil() as usize;
+		sort(&mut data[0..m2]);
+		sort(&mut data[m2 - 1..n]);
+		sort(&mut data[0..m2]);
+	}
+
 
 }
 
@@ -17,6 +24,6 @@ fn print_vec<T>(v: &mut [T]) where T: Debug{
 
 
 fn main() {
-	let mut v: Vec<i32> = vec![1,3,4,2,5];
+	let mut v: Vec<i32> = vec![1337 ,1738, 69, 420, 9001];
 	sort(&mut v);
 }
